f98ffc1422abb4040de7d5e36629919a0e8c632b
Olegshkradiuk <Olegwills40@gmail.com>
2025-04-26 15:29:44 +0300

    hw 31 solutiion

--- src/Store.ts
+++ src/Store.ts
@@ -1,27 +1,30 @@
-import Identifiable from "./Identifiable";
-import Predicate from "./Predicate";
+
+import Identifiable from "./Identifiable.ts";
+import Predicate from "./Predicate.ts";
 
 export default class Store<T extends Identifiable> {
-    items: T[] = [];
-    add(item: T):void {
-       //TODO
-       //adds new item with validation of unique id value
-       //if item with the given id exists the method throws exception 
+  private items: T[] = [];
+
+  add(item: T): void {
+    const exists = this.items.find(i => i.id === item.id);
+    if (exists) {
+      throw new Error(`Item with id "${item.id}" already exists.`);
     }
-    getById(id: string): T | undefined {
-        //TODO
-        //returns item with the given id or undefined if such item doesn't exist
-        return undefined;
-    }
-    find(predicate: Predicate<T>): T[] {
-        //TODO
-        //returns array of items matching the given predicate
-        return [];
-    }
-    remove(id: string): T | undefined {
-        //TODO
-        //removes item with the given id value
-        //returns a reference to being removed item or undefined if an item doesn't exist
-        return undefined;
-    }
+    this.items.push(item);
+  }
+
+  getById(id: string): T | undefined {
+    return this.items.find(item => item.id === id);
+  }
+
+  find(predicate: Predicate<T>): T[] {
+    return this.items.filter(item => predicate.test(item));
+  }
+
+  remove(id: string): T | undefined {
+    const index = this.items.findIndex(item => item.id === id);
+    if (index === -1) return undefined;
+    const [removed] = this.items.splice(index, 1);
+    return removed;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,72 @@
-//TODO
-//Write simple test based on console.log functionality for testing Store class methods
+import Store from "./Store.ts";
 
-import Store from "./Store";
-
-
-
-//For example T msay be class Employee like
 class Employee {
-    constructor(public id: string, public name: string, public salary: number) {
+    constructor(public id: string, public name: string, public salary: number) { }
+}
 
+const store = new Store<Employee>();
+
+document.getElementById('addEmployeeBtn')?.addEventListener('click', () => {
+    const id = (document.getElementById('addId') as HTMLInputElement).value;
+    const name = (document.getElementById('addName') as HTMLInputElement).value;
+    const salary = parseFloat((document.getElementById('addSalary') as HTMLInputElement).value);
+
+    const newEmployee = new Employee(id, name, salary);
+    store.add(newEmployee);
+
+    (document.getElementById('addId') as HTMLInputElement).value = '';
+    (document.getElementById('addName') as HTMLInputElement).value = '';
+    (document.getElementById('addSalary') as HTMLInputElement).value = '';
+
+    updateResult(`Employee added: ${name} (${id}) with salary ${salary}`);
+});
+
+document.getElementById('findEmployeeBtn')?.addEventListener('click', () => {
+    const id = (document.getElementById('findId') as HTMLInputElement).value;
+    const employee = store.getById(id);
+
+    if (employee) {
+        updateResult(`Employee found: ${employee.name}, Salary: ${employee.salary}`);
+    } else {
+        updateResult("Employee not found.");
+    }
+
+    (document.getElementById('findId') as HTMLInputElement).value = '';
+});
+
+document.getElementById('removeEmployeeBtn')?.addEventListener('click', () => {
+    const id = (document.getElementById('removeId') as HTMLInputElement).value;
+    const removedEmployee = store.remove(id);
+
+    if (removedEmployee) {
+        updateResult(`Employee removed: ${removedEmployee.name}`);
+    } else {
+        updateResult("Employee not found for removal.");
+    }
+
+    (document.getElementById('removeId') as HTMLInputElement).value = '';
+});
+
+document.getElementById('getEmployeeBtn')?.addEventListener('click', () => {
+    const id = (document.getElementById('getById') as HTMLInputElement).value;
+    const employee = store.getById(id);
+
+    if (employee) {
+        updateResult(`Employee details: ID: ${employee.id}, Name: ${employee.name}, Salary: ${employee.salary}`);
+    } else {
+        updateResult("Employee not found.");
+    }
+
+    (document.getElementById('getById') as HTMLInputElement).value = '';
+});
+
+function updateResult(message: string) {
+    const resultDiv = document.getElementById('result');
+    if (resultDiv) {
+        resultDiv.innerHTML = message;
     }
 }
-const store = new Store<Employee> ();
-//some test methods
+
+store.add(new Employee("1", "Alice", 5000));
+store.add(new Employee("2", "Bob", 7000));
+store.add(new Employee("3", "Charlie", 3000));
